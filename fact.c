8524c0d1eaf4e0d57de6cacb971f01294fb12490
Cloud User <ec2-user@ip-172-31-15-53.ap-south-1.compute.internal>
2025-03-03 12:09:37 +0000

    Adding c files

---
+++ fact.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void fact() {
+    int number, factorial = 1;
+
+    printf("\n\nEnter a number: ");
+    scanf("%d", &number);
+
+    while(number > 1) {
+        factorial = factorial * number;
+        number--;
+    }
+
+    printf("Factorial is: %d", factorial);
+//    return 0;
+}
---
+++ makefile
@@ -0,0 +1,8 @@
+HELLO.exe:main.o big3.o fact.o
+	gcc -o HELLO.exe main.o big3.o fact.o
+main.o:main.c
+	gcc -c main.c
+big3.o:big3.c
+	gcc -c big3.c
+fact.o:fact.c
+	gcc -c fact.c
